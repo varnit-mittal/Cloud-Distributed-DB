040cd0926212cd21124c47156aab7e51bed7e47b
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2025-11-24 04:10:21 +0530

    done

--- controller/etcd_client.py
+++ controller/etcd_client.py
@@ -3,16 +3,20 @@ import etcd3
 import json
 from typing import Dict, List
 
+import os
+ETCD_HOST = os.environ.get("ETCD_HOST", "etcd")
+ETCD_PORT = int(os.environ.get("ETCD_PORT", "2379"))
+
 class EtcdClient:
-    def __init__(self, host="127.0.0.1", port=2379):
-        self.client = etcd3.client(host=host, port=port)
+    def __init__(self, host=ETCD_HOST, port=ETCD_PORT):
+        self.client = etcd3.client(host=ETCD_HOST, port=ETCD_PORT)
 
     def save_workers(self, workers: Dict[str, str]):
         # workers: {node_id: address}
         self.client.put("../workers", json.dumps(workers))
 
     def load_workers(self) -> Dict[str, str]:
-        v, _ = self.client.get("../workers")
+        v, _ = self.client.get("/workers")
         if not v:
             return {}
         return json.loads(v.decode())
--- scripts/genProto.sh
+++ scripts/genProto.sh
@@ -2,5 +2,14 @@
 set -e
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$ROOT_DIR"/proto
-python3 -m grpc_tools.protoc -I. --python_out=.. --grpc_python_out=.. kv.proto
-echo "Generated Python gRPC modules in project root (kv_pb2.py, kv_pb2_grpc.py)"
+python3 -m grpc_tools.protoc \
+    -I. \
+    --python_out=../worker \
+    --grpc_python_out=../worker \
+    kv.proto
+
+python3 -m grpc_tools.protoc \
+    -I. \
+    --python_out=../controller \
+    --grpc_python_out=../controller \
+    kv.proto
